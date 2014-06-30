34f3570d9534b7cfe12b49f6ca9d1e0b7a595fe0
Kaik541 <shawn.alty@gmail.com>
2014-06-29 20:52:21 -0500

    DPI to 480
    Change DPI back to real value, too many minor inconsistencies

--- g2.mk
+++ g2.mk
@@ -120,7 +120,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.sf.lcd_density=380 \
+        ro.sf.lcd_density=480 \
 	ro.opengles.version=196608 \
 	ro.loki_enabled=1
 
